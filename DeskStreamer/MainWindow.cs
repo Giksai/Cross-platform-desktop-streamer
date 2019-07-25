92480e822f473f3781dd1ac3366aeb650709ea90
Giksai <vermione9@gmail.com>
2019-07-25 21:07:10 +0300

    Upd

--- DeskStreamer/MainWindow.cs
+++ DeskStreamer/MainWindow.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Threading.Tasks;
+using System.Collections.Generic;
 using Gtk;
 
 public partial class MainWindow : Gtk.Window
@@ -6,6 +8,20 @@ public partial class MainWindow : Gtk.Window
     public MainWindow() : base(Gtk.WindowType.Toplevel)
     {
         Build();
+
+        Button btn1 = new Button(Stock.About);
+        Button btn2 = new Button("Cool button");
+        Button btn3 = new Button(Stock.Close);
+        btn3.Pressed += (obj, args) =>
+        {
+            Application.Quit();
+        };
+        Fixed fx1 = new Fixed();
+        fx1.Put(btn1, 0, 0);
+        fx1.Put(btn2, 0, 100);
+        fx1.Put(btn3, 10, 300);
+        Add(fx1);
+        fx1.ShowAll();
     }
 
     protected void OnDeleteEvent(object sender, DeleteEventArgs a)
